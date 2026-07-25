bbc716796646411a10bd9a5b2703f52c719d67e5
Guillermo <panahermoso10@gmail.com>
2026-07-25 07:52:04 -0600

    typing error fixed

--- src/screen/HeroSection.tsx
+++ src/screen/HeroSection.tsx
@@ -39,8 +39,8 @@ export const Hero = () => (
                       className="
                         bg-white text-black font-medium
                         rounded-full
-                        text-md px-4 py-2          /* base: muy compacto, mobile S */
-                        sm:text-lg sm:px-10 sm:py-3 /* sm+: grande como antes */
+                        text-md px-4 py-2         
+                        sm:text-lg sm:px-10 sm:py-3
                         whitespace-nowrap
                         hover:bg-gray-200 hover:shadow-inner
                         transition-all duration-300
